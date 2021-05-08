09567a68919a658d1f59710e3bcb1c5c9a7b9916
pyavchik <pyavchik@gmail.com>
2021-05-08 12:39:37 +0300

    Add method create, remove, delete elements, add reading from file and count letters frequency

---
+++ src/com/company/Main.java
@@ -0,0 +1,80 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.IntStream;
+
+
+public class Main {
+
+    public static final String REGEX = "[!\\.,\\s-\"’'\\d]";
+
+    public static void main(String[] args) throws IOException {
+
+        createPutDelete();
+
+        Map<String, Integer> map = new HashMap<>();
+
+        final Path path = Paths.get("Story.txt");
+
+        final List<String> lines = Files.readAllLines(path);
+
+        lines.stream()
+                .map(str ->str.replaceAll(REGEX, "").toLowerCase())
+                .forEach(str -> {
+                            IntStream.range(0, str.length()).forEach(index -> {
+                                final String symbol = String.valueOf(str.charAt(index));
+
+                                int count = 1;
+
+                                if (map.containsKey(symbol)) {
+                                    count = map.get(symbol);
+                                    count += 1;
+                                }
+
+                                map.put(symbol, count);
+                            });
+                        }
+                );
+
+        class ForExample {
+            private int count;
+            private String word;
+
+            public ForExample(Map.Entry<String, Integer> entry) {
+                count = entry.getValue();
+                word = entry.getKey();
+            }
+
+            @Override
+            public String toString() {
+                return "count=" + count +
+                        ", word='" + word + '\'' +
+                        '}';
+            }
+        }
+
+        Comparator<ForExample> comparator = (fe1, fe2) -> Integer.compare(fe1.count, fe2.getCount());
+
+        map.entrySet()
+                .stream()
+                .map(ForExample::new)
+                .sorted(comparator.reversed())
+                .forEach(System.out::println);
+
+    }
+
+    static void createPutDelete() {
+        List<Integer> list = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        list.remove(0);
+        list.remove(0);
+        list.remove(list.size() - 1);
+        System.out.println(list);
+    }
+}
